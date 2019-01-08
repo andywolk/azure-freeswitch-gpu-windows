5627ac14db82ee351d2d1d5c371051cbe11c35a9
Andrey Volk <andrey@signalwire.com>
2019-01-08 19:28:48 +0400

    Add Windows custom script to install PEM certificate for Verto.

---
+++ pemparametertofile.ps1
@@ -0,0 +1,8 @@
+<# Custom Script for Windows to install a PEM file from Azure ARM template parameter #>
+param (
+    [string]$pemdata
+)
+
+$dest = "C:\Program Files\FreeSWITCH\cert"
+New-Item -Path $dest -ItemType directory
+$pemdata | Out-File "$dest\verto.pem"
