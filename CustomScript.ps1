9150a4378a35c79d53b3f8096a6a6e03f4500fda
Andrey Volk <andrey@signalwire.com>
2019-01-11 02:03:11 +0400

    Start FreeSWITCH service at the end.

--- CustomScript.ps1
+++ CustomScript.ps1
@@ -8,7 +8,7 @@ param (
 	[string]$dnszoneresourcegroup,
     [string]$httpuser="anonymous",
     [string]$httppass="",
-	[string]$msipackagesource
+	[string]$msipackagesource,
 	[string]$freeswitchmsifile
 )
 
@@ -61,3 +61,8 @@ Else
 { 
     "1. Local Administrator software is already existing" 
 }
+
+<# Enable FreeSWITCH service to start with the system #>
+Set-Service -Name "FreeSWITCH" -StartupType Automatic
+<# Start FreeSWITCH service! #>
+Start-Service -Name "FreeSWITCH"
