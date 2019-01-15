0360e093a184c3d436de5aa62c6e2004288b14fe
Andrey Volk <andywolk@gmail.com>
2019-01-15 18:11:40 +0400

    Open ports in Windows Firewall via PowerShell.

--- CustomScript.ps1
+++ CustomScript.ps1
@@ -8,6 +8,9 @@ param (
 	[string]$freeswitchmsifile
 )
 
+New-NetFirewallRule -DisplayName 'FreeSWITCH Server ports' -Direction Inbound -Action Allow -Protocol TCP -LocalPort @('8082', '10000-30000')
+New-NetFirewallRule -DisplayName 'FreeSWITCH Monitoring port' -Direction Inbound -Action Allow -Protocol TCP -LocalPort @('8088')
+
 <# Create a folder for a PEM file #>
 $pemdest = "C:\Program Files\FreeSWITCH\cert"
 New-Item -Path $pemdest -ItemType directory
